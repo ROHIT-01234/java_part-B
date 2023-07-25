10dd60003096b8184d92645842f9a66e6d12acca
ROHIT-01234 <120010829+ROHIT-01234@users.noreply.github.com>
2023-07-26 01:03:57 +0530

    Update Video.java

--- Video.java
+++ Video.java
@@ -1,57 +1,29 @@
-import java.util.List;
-import java.util.Scanner;
-import java.util.ArrayList;
-public class VideoMethods{
-	List<Video> MovieList = new ArrayList<Video>();
-	public void AddMovies() {
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter the name of the movie:");
-		String mName = in.nextLine();
-		System.out.print("Enter the status of the movie(True/False):");
-		boolean status = in.nextBoolean();
-		System.out.print("Enter the ratings for the movie(0-5):");
-		double rating = in.nextDouble();
-		Video v = new Video(mName, status, rating);
-		MovieList.add(v);
-		System.out.println("Library Initialized");
+public class Video {
+	String Name;
+	boolean status;
+	double rating;
+	public Video(String n, boolean s, double r) {
+		super();
+		this.Name = n;
+		this.status = s;
+		this.rating = r;
 	}
-
-	public void DisplayAll() {
-		if (MovieList.isEmpty()) {
-			System.out.println("No movies in the library");
-		}
-		for (Video m : MovieList) {
-			System.out.println("Movie : " + m.getmName() + "  " + "Status : " + m.isStatus() + " " + "Rating " + m.getRating());
-		}
+	public String getmName() {
+		return Name;
 	}
-
-	boolean RentOut(String name)
-	{
- 		for(Video m :MovieList){
-			if(m.getmName().equalsIgnoreCase(name)){
-				if(m.isStatus()){
-					m.setStatus(false);
-					return true;
-				}		
-			}
-			return false;
-		}
-		return false;
+	public void setmName(String mName) {
+		this.Name = mName;
 	}
-
-	public void CollectIn(String name,double rat)
-	{
- 		boolean flag=false;
- 		for(Video m :MovieList)
- 		{
- 			if(m.getmName().equalsIgnoreCase(name)){
-	 			m.setStatus(true);
- 				flag=true;
- 				Math.round(m.setRating((m.getRating() + rat)/2));
-			}
-		}
- 		if(!flag){
-	 		System.out.println("Requested Movie not rented out");
-		} 
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public double setRating(double rating) {
+		return this.rating = rating;
 	}
 }
